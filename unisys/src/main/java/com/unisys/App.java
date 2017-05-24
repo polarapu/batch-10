68c2a5c229aa48c10e88fdd5d30c99063e41800c
muraliee <muraliee.k@gmail.com>
2017-05-24 23:04:53 +0530

    unisys project

---
+++ unisys/src/main/java/com/unisys/App.java
@@ -0,0 +1,13 @@
+package com.unisys;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
